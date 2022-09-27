a6af7f1174701774f8853c2a05690119000bcf9e
Master2H <krushmaster1@gmail.com>
2022-09-27 15:45:57 +0900

    20220927-1

---
+++ baekjoon practice.py
@@ -0,0 +1,44 @@
+# 4673
+'''
+def d(N):
+    ssj = [int(digit) for digit in str(N)]
+    return sum(ssj) + N
+
+NotSelfnumber = [d(x) for x in range(1,10001) if d(x) <= 10000]
+
+for number in range(1,10001):
+    if number not in NotSelfnumber:
+        print(number)
+'''
+
+# 1065
+'''
+N = int(input())
+
+def Check_hansu(n):
+    hansu = 0
+    if n < 100:
+        return print(n)
+    elif n <= 1000:
+        for number in range(100, n+1):
+            number = str(number)
+            if int(number[0]) - int(number[1]) == int(number[1]) - int(number[2]):
+                hansu += 1
+        return print(99 + hansu)
+
+Check_hansu(N)
+'''
+
+# 11654
+'''
+Input = input()
+print(ord(Input))
+'''
+
+# 10809
+'''
+S = input()
+alphabets = "abcdefghijklmnopqrstuvwxyz"
+for alphabet in alphabets:
+    print(S.find(alphabet), end=" ")
+'''
