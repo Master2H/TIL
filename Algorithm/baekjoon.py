c6acc767a98aab703b494049b97af3ece0358ec8
Master2H <krushmaster1@gmail.com>
2023-03-17 13:36:35 +0900

    20230317-06

--- Algorithm/baekjoon.py
+++ Algorithm/baekjoon.py
@@ -580,3 +580,13 @@ print(f"""{n**3}
 3""")
 '''
 
+# 24267
+'''
+n = int(input())
+answer = []
+for i in range(1,n-1):
+    answer.append(int(0.5 * (n-i) * (n-(i+1))))
+print(f"""{sum(answer)}
+3""")
+'''
+
