2b92cb70baa40a72e66363af4a6b9859d6d990f6
Master2H <krushmaster1@gmail.com>
2023-03-31 17:28:10 +0900

    20230331-01

--- Algorithm/baekjoon.py
+++ Algorithm/baekjoon.py
@@ -840,3 +840,45 @@ for i in NumList:
     print(dic[i], end = ' ')
 '''
 
+# 2745
+'''
+N, B = map(str, input().split())
+dic = {"A" : 10,
+       "B" : 11,
+       "C" : 12,
+       "D" : 13,
+       "E" : 14,
+       "F" : 15,
+       "G" : 16,
+       "H" : 17,
+       "I" : 18,
+       "J" : 19,
+       "K" : 20,
+       "L" : 21,
+       "M" : 22,
+       "N" : 23,
+       "O" : 24,
+       "P" : 25,
+       "Q" : 26,
+       "R" : 27,
+       "S" : 28,
+       "T" : 29,
+       "U" : 30,
+       "V" : 31,
+       "W" : 32,
+       "X" : 33,
+       "Y" : 34,
+       "Z" : 35,
+       }
+answer = 0
+for i in range(len(N)):
+    try:
+        temp = int(N[i])*int(B)**(len(N)-(i+1))
+        answer += temp
+    except:
+        temp = dic[N[i]]*int(B)**(len(N)-(i+1))
+        answer += temp
+
+print(answer)
+'''
+
