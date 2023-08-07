08b0b9d48b4ea2ca11c436c491f0bb074cc0d590
Master2H <krushmaster1@gmail.com>
2023-08-07 20:05:32 +0900

    20230807-01

--- Algorithm/baekjoon.py
+++ Algorithm/baekjoon.py
@@ -1445,3 +1445,32 @@ for i in answer:
     print(i[0])
 '''
 
+# 10828
+'''
+import sys
+input = sys.stdin.readline
+stack = []
+N = int(input())
+for _ in range(N):
+    cmd = input().split()
+    if len(cmd) == 2:
+        stack.append(cmd[1])
+    if cmd[0] == 'pop':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(int(stack.pop(-1)))
+    if cmd[0] == 'size':
+        print(len(stack))
+    if cmd[0] == 'empty':
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+    if cmd[0] == 'top':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])
+'''
+
