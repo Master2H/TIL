af70ff6e288fbe441788feabe3b44075f236e8d4
Master2H <krushmaster1@gmail.com>
2023-08-07 23:25:34 +0900

    20230807-07

--- Algorithm/baekjoon.py
+++ Algorithm/baekjoon.py
@@ -1620,3 +1620,40 @@ while numList:
 print('Nice' if not stack else 'Sad')
 '''
 
+# 18258
+'''
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+que = deque()
+N = int(input())
+for _ in range(N):
+    cmd = input().split()
+    
+    if len(cmd) >= 2:
+        que.append(int(cmd[1]))
+    elif cmd[0] == 'pop':
+        if que:
+            print(que.popleft())
+        else:
+            print(-1)
+    elif cmd[0] == 'size':
+        print(len(que))
+    elif cmd[0] == 'empty':
+        if que:
+            print(0)
+        else:
+            print(1)
+    elif cmd[0] == 'front':
+        if que:
+            print(que[0])
+        else:
+            print(-1)
+    elif cmd[0] == 'back':
+        if que:
+            print(que[-1])
+        else:
+            print(-1)
+'''
+
