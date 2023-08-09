8697fa39ddb6885342132ed12a91cb04a9b59a4a
Master2H <krushmaster1@gmail.com>
2023-08-09 20:12:16 +0900

    20230809-02

--- Algorithm/baekjoon.py
+++ Algorithm/baekjoon.py
@@ -1703,3 +1703,47 @@ while numList:
 print('<', ', '.join(answer), '>', sep ='')
 '''
 
+# 28279
+'''
+from collections import deque
+import sys
+input = sys.stdin.readline
+deq = deque()
+N = int(input())
+
+for _ in range(N):
+    cmd = input().split()
+    
+    if cmd[0] == '1':
+        deq.appendleft(int(cmd[1]))
+    if cmd[0] == '2':
+        deq.append(int(cmd[1]))
+    if cmd[0] == '3':
+        if deq:
+            print(deq.popleft())
+        else:
+            print(-1)
+    if cmd[0] == '4':
+        if deq:
+            print(deq.pop())
+        else:
+            print(-1)
+    if cmd[0] == '5':
+        print(len(deq))
+    if cmd[0] == '6':
+        if deq:
+            print(0)
+        else:
+            print(1)
+    if cmd[0] == '7':
+        if deq:
+            print(deq[0])
+        else:
+            print(-1)
+    if cmd[0] == '8':
+        if deq:
+            print(deq[-1])
+        else:
+            print(-1)
+'''
+
