8e407eb68e638bacd1703159523e74f44bc437e7
Master2H <krushmaster1@gmail.com>
2023-10-20 10:01:50 +0900

    20231020-03

--- Algorithm/baekjoon.py
+++ Algorithm/baekjoon.py
@@ -1996,3 +1996,39 @@ def dfs(start):
 
 dfs(1)
 '''
+
+# 15651
+'''
+N, M = map(int, input().split())
+
+s = []
+def dfs():
+    if len(s) == M:
+        print(' '.join(map(str, s)))
+        return
+    
+    for i in range(1, N+1):
+        s.append(i)
+        dfs()
+        s.pop()
+
+dfs()
+'''
+
+# 15652
+'''
+N, M = map(int, input().split())
+s = []
+def dfs(start):
+    if len(s) == M:
+        print(' '.join(map(str, s)))
+        return
+    
+    for i in range(start, N+1):
+        s.append(i)
+        dfs(i)
+        s.pop()
+
+dfs(1)
+'''
+
